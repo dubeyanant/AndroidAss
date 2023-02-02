5b777ae13156305cc6004e27fc4241ad1dd22240
Anant Dubey <anantkumar312@gmail.com>
2023-02-03 01:04:34 +0530

    added another activity

--- app/src/main/java/com/example/androidass/MainActivity.java
+++ app/src/main/java/com/example/androidass/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.androidass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button fifth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fifth = (Button) findViewById(R.id.fifth);
+        fifth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), feb1_1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/androidass/feb1_1.java
@@ -0,0 +1,14 @@
+package com.example.androidass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class feb1_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feb11);
+    }
+}
