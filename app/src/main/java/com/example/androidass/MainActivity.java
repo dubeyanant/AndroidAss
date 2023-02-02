b253e8737073ba7c8f5a073ca7cfdf5649574a2d
Anant Dubey <anantkumar312@gmail.com>
2023-02-03 01:47:43 +0530

    added 1st feb q2

--- app/src/main/java/com/example/androidass/MainActivity.java
+++ app/src/main/java/com/example/androidass/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button f11;
+    Button f11, f12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         f11 = (Button) findViewById(R.id.f11);
+        f12 = (Button) findViewById(R.id.f12);
+
         f11.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), feb1_1.class);
+                startActivity(intent);
+            }
+        });
+        f12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), feb1_2.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/androidass/feb1_2.java
@@ -0,0 +1,72 @@
+package com.example.androidass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
+
+public class feb1_2 extends AppCompatActivity {
+
+    ImageSwitcher imageSwitcher;
+    TextView txt;
+    Button b1, b2;
+    int animalPics[] = {R.drawable.cats, R.drawable.doggo, R.drawable.lamba, R.drawable.monki, R.drawable.panda, R.drawable.rabbit, R.drawable.sheep};
+    String animalNames[] = {"Cat", "Dog", "Lamba", "Monkey", "Panda", "Rabbit", "Sheep"};
+    int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feb12);
+
+        imageSwitcher = (ImageSwitcher) findViewById(R.id.imageSwitcher);
+        txt = (TextView) findViewById(R.id.textView);
+        b1 = (Button) findViewById(R.id.prev);
+        b2 = (Button) findViewById(R.id.next);
+
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView myView = new ImageView(getApplicationContext());
+                myView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                myView.setLayoutParams(new
+                        ImageSwitcher.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,
+                        ActionBar.LayoutParams.WRAP_CONTENT));
+                return myView;
+            }
+        });
+
+        imageSwitcher.setImageResource(animalPics[0]);
+        txt.setText(animalNames[0]);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (i == 0)
+                    i = animalPics.length - 1;
+                else
+                    i--;
+                imageSwitcher.setImageResource(animalPics[i]);
+                txt.setText(animalNames[i]);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (i == animalPics.length - 1)
+                    i = 0;
+                else
+                    i++;
+                imageSwitcher.setImageResource(animalPics[i]);
+                txt.setText(animalNames[i]);
+            }
+        });
+    }
+}
