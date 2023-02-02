3a037f4e9e0f203d8992c48a5479d3cd1602cf3b
Anant Dubey <anantkumar312@gmail.com>
2023-02-03 01:09:31 +0530

    added 1st feb ass

--- app/src/main/java/com/example/androidass/feb1_1.java
+++ app/src/main/java/com/example/androidass/feb1_1.java
@@ -1,14 +1,66 @@
 package com.example.androidass;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class feb1_1 extends AppCompatActivity {
+
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feb11);
+
+        txt = (TextView) findViewById(R.id.txt);
+    }
+
+    public void showAlertDialogButtonClicked(View view) {
+
+        // Create an alert builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter value of n:");
+
+        // set the custom layout
+        final View customLayout = getLayoutInflater().inflate(R.layout.custom_layout, null);
+        builder.setView(customLayout);
+
+        // add a button
+        builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // send data from the
+                // AlertDialog to the Activity
+                EditText editText = customLayout.findViewById(R.id.editText);
+                sendDialogDataToActivity(editText.getText().toString());
+            }
+        });
+
+        // create and show
+        // the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    // Do something with the data
+    // coming from the AlertDialog
+    private void sendDialogDataToActivity(String data) {
+        int n = Integer.parseInt(data); // Converting string data to int n
+        String temp = "";
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                temp = temp + Integer.toString(i) + " ";
+            }
+        }
+
+        txt.setText(temp);
     }
 }
