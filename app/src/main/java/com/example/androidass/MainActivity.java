09d278fdeac0768a77df0202aa9b1220ee2b3dfa
Anant Dubey <anantkumar312@gmail.com>
2023-02-19 14:58:35 +0530

    cleaned MainActivity.java

--- app/src/main/java/com/example/androidass/MainActivity.java
+++ app/src/main/java/com/example/androidass/MainActivity.java
@@ -12,181 +12,65 @@ public class MainActivity extends AppCompatActivity {
 
     EditText name, prn;
     String Name, PRN;
-    Button j121, j122, j181, j182, j191, j192, j251, j252, j253, f11, f12, f81, f161;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        j121 = (Button) findViewById(R.id.j121);
-        j122 = (Button) findViewById(R.id.j122);
-        j181 = (Button) findViewById(R.id.j181);
-        j182 = (Button) findViewById(R.id.j182);
-        j191 = (Button) findViewById(R.id.j191);
-        j192 = (Button) findViewById(R.id.j192);
-        j251 = (Button) findViewById(R.id.j251);
-        j252 = (Button) findViewById(R.id.j252);
-        j253 = (Button) findViewById(R.id.j253);
-        f11 = (Button) findViewById(R.id.f11);
-        f12 = (Button) findViewById(R.id.f12);
-        f81 = (Button) findViewById(R.id.f81);
-        f161 = (Button) findViewById(R.id.f161);
+        Button[] buttons = new Button[]{
+                findViewById(R.id.j121),
+                findViewById(R.id.j122),
+                findViewById(R.id.j181),
+                findViewById(R.id.j182),
+                findViewById(R.id.j191),
+                findViewById(R.id.j192),
+                findViewById(R.id.j251),
+                findViewById(R.id.j252),
+                findViewById(R.id.j253),
+                findViewById(R.id.f11),
+                findViewById(R.id.f12),
+                findViewById(R.id.f81),
+                findViewById(R.id.f161)
+        };
 
-        j121.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                find();
-                Intent intent = new Intent(v.getContext(), jan12_1.class);
-                intent.putExtra("Name", Name);
-                intent.putExtra("PRN", PRN);
-                startActivity(intent);
-            }
-        });
+        Intent[] intents = new Intent[] {
+                new Intent(this, jan12_1.class),
+                new Intent(this, jan12_2.class),
+                new Intent(this, jan18_1.class),
+                new Intent(this, jan18_2.class),
+                new Intent(this, jan19_1.class),
+                new Intent(this, jan19_2.class),
+                new Intent(this, jan25_1.class),
+                new Intent(this, jan25_2.class),
+                new Intent(this, jan25_3.class),
+                new Intent(this, feb1_1.class),
+                new Intent(this, feb1_2.class),
+                new Intent(this, feb8_1.class),
+                new Intent(this, feb16_1.class)
+        };
 
-        j122.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                find();
-                Intent intent = new Intent(v.getContext(), jan12_2.class);
-                intent.putExtra("Name", Name);
-                intent.putExtra("PRN", PRN);
-                startActivity(intent);
-            }
-        });
+        for (int i = 0; i < buttons.length; i++) {
+            final int index = i;
+            buttons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    name = (EditText) findViewById(R.id.name);
+                    prn = (EditText) findViewById(R.id.prn);
 
-        j181.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                find();
-                Intent intent = new Intent(v.getContext(), jan18_1.class);
-                intent.putExtra("Name", Name);
-                intent.putExtra("PRN", PRN);
-                startActivity(intent);
-            }
-        });
+                    if (name.getText().toString().equals("") || prn.getText().toString().equals("")) {
+                        Name = getString(R.string.Name);
+                        PRN = getString(R.string.PRN);
+                    } else {
+                        Name = name.getText().toString();
+                        PRN = prn.getText().toString();
+                    }
 
-        j182.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                find();
-                Intent intent = new Intent(v.getContext(), jan18_2.class);
-                intent.putExtra("Name", Name);
-                intent.putExtra("PRN", PRN);
-                startActivity(intent);
-            }
-        });
-
-        j191.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                find();
-                Intent intent = new Intent(v.getContext(), jan19_1.class);
-                intent.putExtra("Name", Name);
-                intent.putExtra("PRN", PRN);
-                startActivity(intent);
-            }
-        });
-
-        j192.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                find();
-                Intent intent = new Intent(v.getContext(), jan19_2.class);
-                intent.putExtra("Name", Name);
-                intent.putExtra("PRN", PRN);
-                startActivity(intent);
-            }
-        });
-
-        j251.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                find();
-                Intent intent = new Intent(v.getContext(), jan25_1.class);
-                intent.putExtra("Name", Name);
-                intent.putExtra("PRN", PRN);
-                startActivity(intent);
-            }
-        });
-
-        j252.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                find();
-                Intent intent = new Intent(v.getContext(), jan25_2.class);
-                intent.putExtra("Name", Name);
-                intent.putExtra("PRN", PRN);
-                startActivity(intent);
-            }
-        });
-
-        j253.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                find();
-                Intent intent = new Intent(v.getContext(), jan25_3.class);
-                intent.putExtra("Name", Name);
-                intent.putExtra("PRN", PRN);
-                startActivity(intent);
-            }
-        });
-
-        f11.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                find();
-                Intent intent = new Intent(v.getContext(), feb1_1.class);
-                intent.putExtra("Name", Name);
-                intent.putExtra("PRN", PRN);
-                startActivity(intent);
-            }
-        });
-
-        f12.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                find();
-                Intent intent = new Intent(v.getContext(), feb1_2.class);
-                intent.putExtra("Name", Name);
-                intent.putExtra("PRN", PRN);
-                startActivity(intent);
-            }
-        });
-
-        f81.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                find();
-                Intent intent = new Intent(v.getContext(), feb8_1.class);
-                intent.putExtra("Name", Name);
-                intent.putExtra("PRN", PRN);
-                startActivity(intent);
-            }
-        });
-
-        f161.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                find();
-                Intent intent = new Intent(v.getContext(), feb16_1.class);
-                intent.putExtra("Name", Name);
-                intent.putExtra("PRN", PRN);
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void find() {
-        name = (EditText) findViewById(R.id.name);
-        prn = (EditText) findViewById(R.id.prn);
-
-        if(name.getText().toString().equals("") || prn.getText().toString().equals("")) {
-            Name = getString(R.string.Name);
-            PRN = getString(R.string.PRN);
-        } else {
-            Name = name.getText().toString();
-            PRN = prn.getText().toString();
+                    intents[index].putExtra("Name", Name);
+                    intents[index].putExtra("PRN", PRN);
+                    startActivity(intents[index]);
+                }
+            });
         }
     }
 }
